39dfae2e33492eecdc76b3d33cb5e108b44a1e98
godhj93 <godhj@unist.ac.kr>
2022-05-31 12:59:11 +0000

    fix eval.py

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import argparse
-from utils.data import data_load
-from tensorflow.keras.losses import SparseCategoricalCrossentropy as Crossentropy
-from tensorflow.keras.metrics import SparseCategoricalAccuracy as CategoricalAccuracy
+from utils.data import data_loader
+from tensorflow.keras.losses import CategoricalCrossentropy as Crossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy
 gpus = tf.config.experimental.list_physical_devices("GPU")
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu,True)
@@ -10,6 +10,7 @@ for gpu in gpus:
 parser = argparse.ArgumentParser('Evaluate MobileViT | Dataset : CIFAR 10')
 parser.add_argument("--arch", default='S', type=str,help="Architecture: [S, XS, XSS]")
 parser.add_argument("--model")
+parser.add_argument("--size", default=256)
 parser.add_argument("--data", default='cifar10')
 args = parser.parse_args()
 
@@ -20,10 +21,11 @@ def test():
     model = tf.keras.models.load_model(args.model)
     print(model.summary())
 
-    _, test_ds = data_load(dataset=args.data, batch_size=32, size=256)
-
+    
+    test_data_loader = data_loader(dataset=args.data, batch_size=8, size=args.size, DEBUG=False)
+    _, test_ds = test_data_loader.load()
     model.compile(
-        loss = Crossentropy(),
+        loss = Crossentropy(from_logits=False,label_smoothing=0.1),
         metrics = CategoricalAccuracy()
     )
 
