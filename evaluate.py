2f5a92325c1142bcb5961c4a411784721bf22ed8
godhj93 <godhj@unist.ac.kr>
2022-06-05 07:00:31 +0000

    edit eval.[y

--- evaluate.py
+++ evaluate.py
@@ -7,11 +7,16 @@ gpus = tf.config.experimental.list_physical_devices("GPU")
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu,True)
 
+from tqdm import tqdm
+import time
+import numpy as np
+
 parser = argparse.ArgumentParser('Evaluate MobileViT | Dataset : CIFAR 10')
 parser.add_argument("--arch", default='S', type=str,help="Architecture: [S, XS, XSS]")
 parser.add_argument("--model")
 parser.add_argument("--size", default=256)
 parser.add_argument("--data", default='cifar10')
+parser.add_argument("--device", default='gpu')
 args = parser.parse_args()
 
 def test():
@@ -22,15 +27,36 @@ def test():
     print(model.summary())
 
     
-    test_data_loader = data_loader(dataset=args.data, batch_size=8, size=args.size, DEBUG=False)
+    test_data_loader = data_loader(dataset=args.data, batch_size=1, size=args.size, DEBUG=False)
     _, test_ds = test_data_loader.load()
-    model.compile(
-        loss = Crossentropy(from_logits=False,label_smoothing=0.1),
-        metrics = CategoricalAccuracy()
-    )
+    # model.compile(
+    #     loss = Crossentropy(from_logits=False,label_smoothing=0.1),
+    #     metrics = CategoricalAccuracy()
+    # )
 
-    model.evaluate(test_ds)
+    # model.evaluate(test_ds)
+
+
     
+    device = args.device
+    
+    
+    print(f"Device: {device}")
+    with tf.device(device):
+            
+        pbar = tqdm(test_ds)
+        acc_fn = tf.keras.metrics.CategoricalAccuracy()
+        latency_list = []
+        for x,y in pbar:
+            t0 = time.time()
+            y_hat = model.predict(x)
+            t1 = time.time()
+            latency_list.append(t1-t0)
+            acc = acc_fn(y, y_hat)
+
+            pbar.set_description(f"acc: {acc:.4f}, latency: {np.mean(latency_list)*1000:.4f}ms")
+            
+
     
 if __name__ == '__main__':
 
